0a4d5e759307e2eb9ed2821f475e4274ddc41ad0
ninhTKieen <ninhtrungkien2811@gmail.com>
2024-07-10 17:31:03 +0700

    feat: add tanstack and axios

--- App.tsx
+++ App.tsx
@@ -4,15 +4,20 @@ import {View} from 'react-native';
 import {Text} from '@rneui/themed';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text h1>Hello World</Text>
-        <Icon name="home" size={30} color="#900" />
-      </View>
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text h1>Hello World</Text>
+          <Icon name="home" size={30} color="#900" />
+        </View>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 };
 
