53a6b89f344c93e033cc215fc9ac2370591723f0
ninhTKieen <ninhtrungkien2811@gmail.com>
2024-07-15 14:17:12 +0700

    feat: rename app

--- android/app/src/main/java/com/rnnativebase/MainApplication.kt
+++ android/app/src/main/java/com/rnnativebase/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rnelementapp
+package com.rnnativebase
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/RnNativeBaseAppTests/RnNativeBaseAppTests.m
+++ ios/RnNativeBaseAppTests/RnNativeBaseAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RnElementAppTests : XCTestCase
+@interface RnNativeBaseAppTests : XCTestCase
 
 @end
 
-@implementation RnElementAppTests
+@implementation RnNativeBaseAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
