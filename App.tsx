4c2745a032abe245ffa63518d0eef7c01613c8dc
ntrungkieen <ninhtrungkien2811@gmail.com>
2024-11-19 21:39:06 +0700

    style: notificated

--- App.tsx
+++ App.tsx
@@ -1,44 +1,17 @@
 import { modalStack } from '@src/configs/modal/modal.config';
+import { NotificationsProvider } from '@src/configs/notificated.config';
 import RootNavigator from '@src/navigations';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { NativeBaseProvider } from 'native-base';
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ModalProvider } from 'react-native-modalfy';
-import {
-  ZoomInDownZoomOutUp,
-  createNotifications,
-} from 'react-native-notificated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 60 * 24,
-    },
-  },
-});
-
-const { NotificationsProvider } = createNotifications({
-  animationConfig: ZoomInDownZoomOutUp,
-  defaultStylesSettings: {
-    globalConfig: {
-      titleSize: 16,
-      titleWeight: '600',
-      descriptionSize: 12,
-      descriptionWeight: '400',
-      multiline: 2,
-      imageStyle: {
-        width: 20,
-        height: 20,
-        justifyContent: 'flex-start',
-        alignSelf: 'flex-start',
-      },
-    },
-    errorConfig: {
-      bgColor: '#FFEDED',
-      accentColor: '#FF4D4F',
-      leftIconSource: require('@src/assets/image.png'),
     },
   },
 });
