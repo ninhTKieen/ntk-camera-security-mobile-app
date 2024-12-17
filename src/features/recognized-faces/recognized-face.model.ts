45629ff1115b558a295102701c67792fb437a4c9
ntrungkieen <ninhtrungkien2811@gmail.com>
2024-12-17 10:22:12 +0700

    chore: type for responseRecognizedFace

--- src/features/recognized-faces/recognized-face.model.ts
+++ src/features/recognized-faces/recognized-face.model.ts
@@ -26,3 +26,72 @@ export type TGetListRecognizedFace = {
   imageUrl: string;
   isActive: boolean;
 } & TFullAudited;
+
+export type TResponseRecognizedFace = {
+  detection: {
+    _imageDims: {
+      _width: number;
+      _height: number;
+    };
+    _score: number;
+    _classScore: number;
+    _className: string;
+    _box: {
+      _x: number;
+      _y: number;
+      _width: number;
+      _height: number;
+    };
+  };
+  landmarks: {
+    _imgDims: {
+      _width: number;
+      _height: number;
+    };
+    _shift: {
+      _x: number;
+      _y: number;
+    };
+    _positions: {
+      _x: number;
+      _y: number;
+    }[];
+  };
+  unshiftedLandmarks: {
+    _imgDims: {
+      _width: number;
+      _height: number;
+    };
+    _shift: {
+      _x: number;
+      _y: number;
+    };
+    _positions: {
+      _x: number;
+      _y: number;
+    }[];
+  };
+  alignedRect: {
+    _imageDims: {
+      _width: number;
+      _height: number;
+    };
+    _score: number;
+    _classScore: number;
+    _className: string;
+    _box: {
+      _x: number;
+      _y: number;
+      _width: number;
+      _height: number;
+    };
+  };
+  angle: {
+    roll: number;
+    pitch: number;
+    yaw: number;
+  };
+  descriptor: {
+    [key: string]: number;
+  };
+};
